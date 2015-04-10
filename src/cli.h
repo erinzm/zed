78e4c085191de85b4c8810e21c1e0f89dc912a9c
Liam Marshall <archimedespi3141@gmail.com>
2015-04-09 21:40:16 -0500

    Bare skeleton of CLI

---
+++ src/cli.h
@@ -0,0 +1,10 @@
+#include <argp.h>
+#include <stdbool.h>
+
+const char *argp_program_version = "compilerinaweek 0.1";
+const char *argp_program_bug_address =
+  "https://github.com/ArchimedesPi/compilerinaweek/issues";
+const int argp_flags = 0;
+static char doc[] =
+  "Compiler In A Week - @archimedespi's personal project to learn about compilers";
+static struct argp argp = {0, 0, 0, doc, 0, 0, 0};
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include "cli.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
+  argp_parse(&argp, argc, argv, argp_flags, 0, 0);
   return 0;
 }
