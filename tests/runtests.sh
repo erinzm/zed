e7f88404c49c4bf4c2c2d36eb2d216781b9f5a53
Liam Marshall <archimedespi3141@gmail.com>
2015-04-14 14:10:58 -0500

    Report and colorize test results

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,4 +1,3 @@
-echo
 echo "Running unit tests"
 for test in test/*_test
 do
@@ -7,11 +6,17 @@ do
 		./$test 2>&1 /tmp/test.log
 		if [[$? -eq 0]]
 		then
+			echo "$(tput setaf 2)$test passed $(tput bold)(✓)$(tput sgr0)"
 			rm -f /tmp/test.log
 		else
+			echo "$(tput setaf 1)$test failed $(tput bold)(✗)$(tput sgr0)"
 			cat /tmp/test.log
 			rm -f /tmp/test.log
 			exit 1
 		fi
 	fi
 done
+
+echo
+echo "$(tput setaf 2)All tests passed! $(tput bold)(✔)$(tput sgr0)"
+exit 0
