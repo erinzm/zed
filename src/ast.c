0361068f3154b6c6678ea3b6bf06fe614f4d6c2e
Liam Marshall <archimedespi3141@gmail.com>
2015-05-03 15:38:48 -0500

    Fix much of ast.c
    Fixed:
    * newlines in printf
    * format strings
    * imports
    * struct field names
    * indentation
    * default cases

--- src/ast.c
+++ src/ast.c
@@ -1,8 +1,10 @@
-#include "ast.h"
-#include "util.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include "util.h"
+
+#include "ast.h"
+
 
 ast_node *ast_number_create(double value) {
 	ast_node *node = STRUCT_INSTANCE(ast_node);
@@ -28,24 +30,26 @@ ast_node *ast_binary_op_create(ast_type_binop op, ast_node *lhs, ast_node *rhs) 
 }
 
 void dump_ast_node(ast_node *node) {
-	printf("=== node dump ===");
+	printf("=== node dump ===\n");
 	switch(node->type) {
 		case AST_TYPE_NUMBER:
 			printf("node->type == AST_TYPE_NUMBER\n");
 			INSPECT(node->number.value, "%f");
 			break;
 		case AST_TYPE_VARIABLE:
-			printf("node->type == AST_TYPE_VARIABLE");
+			printf("node->type == AST_TYPE_VARIABLE\n");
 			INSPECT(node->variable.name, "%s");
 			break;
 		case AST_TYPE_BINARY_OP:
-			printf("node->type == AST_TYPE_BINARY_OP");
+			printf("node->type == AST_TYPE_BINARY_OP\n");
 			INSPECT(node->binary_op.op, "%i");
 			dump_ast_node(node->binary_op.lhs);
 			dump_ast_node(node->binary_op.rhs);
 			break;
+		default:
+			break;
 	}
-	printf("=== end node dump ===");
+	printf("=== end node dump ===\n");
 }
 
 void ast_node_free(ast_node *node) {
@@ -66,8 +70,8 @@ void ast_node_free(ast_node *node) {
 		}
 		case AST_TYPE_FNCALL: {
 			FREE_IF_EXISTS(node->fncall.name);
-			for(unsigned int i=0; i<node->call.arg_count; i++) {
-                ast_node_free(node->call.args[i]); // recursively free those sub-asts
+			for(unsigned int i=0; i<node->fncall.argc; i++) {
+                ast_node_free(node->fncall.args[i]); // recursively free those sub-asts
             }
             free(node->fncall.args);
 		}
