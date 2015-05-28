a9c1a6e4ff4c84a74f8c2ddaeebdc953e31c6db7
Liam Marshall <liam@cpan.org>
2015-05-27 21:30:57 -0500

    Stub codegen

---
+++ src/codegen.h
@@ -0,0 +1,17 @@
+/*
+* ast.h
+* an ast for COMPILER IN A WEEK
+*
+* GPLv3 Liam Marshall 2015
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "util.h"
+
+#ifndef _codegen_h
+#define _codegen_h
+
+#endif
