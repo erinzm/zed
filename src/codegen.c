bcd2ec06c8d75a839420aa0ecb9766f59b7a841a
Liam Marshall <liam@cpan.org>
2015-05-29 17:54:19 -0500

    All the codegen implementations!

--- src/codegen.c
+++ src/codegen.c
@@ -1,2 +1,26 @@
 #include "codegen.h"
 
+char *codegen_number(ast_node *node) {
+  sds s;
+  s = sdsnew("");
+  s = sdscatprintf(s, "%f", node->number.value);
+  return s;
+}
+
+char *codegen_string(ast_node *node) {
+  sds s;
+  s = sdsnew("\"");
+  s = sdscat(s, node->string.value);
+  s = sdscat(s, "\"");
+  return s;
+}
+
+char *codegen_fncall(ast_node *node) {
+  sds s;
+  s = sdsnew(node->fncall.name);
+  s = sdscat(s, "(");
+  s = sdscat(s, ")");
+  return s;
+}
+
+char *codegen(ast_node *root_node);
