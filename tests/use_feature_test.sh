d4e4012f738bb1e53ee85f8836021993348ab011
Liam Marshall <liam@cpan.org>
2015-05-31 11:38:35 -0500

    Zedify tests

--- tests/use_feature_test.sh
+++ tests/use_feature_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e # Error on fail
 
-cat tests/testdata/use.ciaw | ./ciaw
+cat tests/testdata/use.zd | ./zed
--- tests/weird_feature_test.sh
+++ tests/weird_feature_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e # Error on fail
 
-cat tests/testdata/weird.ciaw | ./ciaw
+cat tests/testdata/weird.zd | ./zed
