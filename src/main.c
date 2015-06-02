0878c3eb0f2b873b2fe56311b516583c5246cf00
Liam Marshall <liam@cpan.org>
2015-06-01 19:51:19 -0500

    Begin work on the CLI

--- src/main.c
+++ src/main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#include <argp.h>
 #include <stdbool.h>
+#include "commander/commander.h"
 #include "parser.h"
 #include "ast.h"
 #include "codegen.h"
-
-const char *argp_program_version = "Zed 0.1";
-const char *argp_program_bug_address =
-  "https://github.com/ArchimedesPi/zed/issues";
-const int argp_flags = 0;
-static char doc[] =
-  "zed - a compiler";
-static struct argp argp = {0, 0, 0, doc, 0, 0, 0};
 
 extern int yyparse();
 extern ast_node *parsetree;
 
 int main(int argc, char** argv) {
-  argp_parse(&argp, argc, argv, argp_flags, 0, 0);
+  command_t cmd;
+  command_init(&cmd, argv[0], "0.1");
+  cmd.usage = "[options] <inputfile>";
+  command_parse(&cmd, argc, argv);
+
   yyparse();
   // dump_ast_node(parsetree);
   printf("%s", codegen(parsetree));
