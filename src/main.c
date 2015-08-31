fcd3e96f2eb8f906dceae1c60bd219f1386724a4
Liam Marshall <archimedespi3141@gmail.com>
2015-08-31 17:09:09 -0500

    begin ripping out commander

--- src/main.c
+++ src/main.c
@@ -20,29 +20,9 @@ enum {
 
 bool doStdout = false;
 
-
-void cmdIsDump(command_t *self) {
-  mode = DUMP;
-}
-
-void cmdIsC(command_t *self) {
-  mode = C;
-}
-
-void cmdStdOut(command_t *self) {
-  doStdout = true;
-}
-
 int main(int argc, char** argv) {
   mode = C;
   command_t cmd;
-  command_init(&cmd, argv[0], "0.1");
-  cmd.usage = "[options] <inputfile>";
-  command_option(&cmd, "-d", "--dump", "dump AST", cmdIsDump);
-  command_option(&cmd, "-c", "--c", "output C", cmdIsC);
-  command_option(&cmd, "-s", "--stdout", "output to stdout instead of a file", cmdStdOut);
-  command_parse(&cmd, argc, argv);
-  FILE *source = fopen(cmd.argv[0], "r");
   if (!source) {
     printf("Can't open %s!\n", cmd.argv[0]);
     return 1;
