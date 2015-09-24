a410935ff76b05f371eb5298bd9fd34060c709b2
Liam Marshall <ArchimedesPi@users.noreply.github.com>
2015-09-24 12:35:17 -0500

    util.h: document the utility functions and macros
    Adds documentation for:
     * `FREE_IF_EXISTS(pointer)`
     * `STRUCT_INSTANCE(type)`
     * `STRUCT_WIDE_INSTANCE(type, width)`
     * `INSPECT(thing, fmt)`
     * `char *chopStringTips(char *input)`

--- src/util.h
+++ src/util.h
@@ -3,12 +3,16 @@
 #include <stdio.h>
 #include <string.h>
 
-// free a pointer if it exists
+// free a pointer if it exists.
 #define FREE_IF_EXISTS(pointer) if (pointer) free(pointer)
+// nice instantiator shorthand for structs.
 #define STRUCT_INSTANCE(type) malloc(sizeof(type))
+// same as previous but for "wide" structs - arrays of structs.
 #define STRUCT_WIDE_INSTANCE(type, width) malloc(sizeof(type) * width)
+// inspect a thing - just pass a thing and a printf format string for that thing. great for debug.
 #define INSPECT(thing, fmt) printf(#thing " == " fmt "\n", thing)
 
+// chop the lexed "s off of a string
 char *chopStringTips(char *input);
 
 #endif
