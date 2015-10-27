01a52cad75c19030fb9d852fcaa59aaccbb8c792
Liam Marshall <archimedespi3141@gmail.com>
2015-10-26 23:02:39 -0500

    Remove failing test in zed.compiler-test

--- test/zed/compiler_test.clj
+++ test/zed/compiler_test.clj
@@ -1,7 +1,3 @@
-(ns zed.compiler_test
+(ns zed.compiler-test
   (:require [clojure.test :refer :all]
             [zed.compiler :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
