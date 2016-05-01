feaeb92926009379bb3781b377c41d84cee5cd6b
Liam Marshall <liam@cpan.org>
2016-04-30 19:17:51 -0500

    Fix a few bugs in the testrunner

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+test_failed=false
+
 echo "$(tput setaf 3)Running unit tests$(tput sgr0)"
 for test in tests/*_test*
 do
@@ -11,6 +13,7 @@ do
 			echo "$(tput setaf 2)$test passed $(tput bold)(✓)$(tput sgr0)"
 			rm -f /tmp/test.log
 		else
+      test_failed=true
 			echo "$(tput setaf 1)$test failed $(tput bold)(✗)$(tput sgr0)"
 			echo "$(tput setaf 1)$test's output (stdout+stderror):$(tput sgr0)"
 			cat /tmp/test.log
@@ -20,5 +23,10 @@ do
 done
 
 echo
-echo "$(tput setaf 2)All tests passed! $(tput bold)(✔)$(tput sgr0)"
+if [ "$test_failed" != "true" ]; then
+  echo "$(tput setaf 2)All tests passed! $(tput bold)(✔)$(tput sgr0)"
+else
+  echo "$(tput setaf 1)Some tests failed! $(tput bold)(✓)$(tput sgr0)"
+fi
+
 exit 0
