0ae88c57a830d5559f99bc86ed9241a1aa97556f
Liam Marshall <liam@cpan.org>
2016-04-30 19:18:17 -0500

    Update main.c to use argp

--- src/main.c
+++ src/main.c
@@ -7,8 +7,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <libgen.h>
-#include "commander/commander.h"
+
+#include <argp.h>
+
 #include "sds/sds.h"
+
 #include "parser.h"
 #include "ast.h"
 #include "codegen.h"
@@ -17,18 +20,64 @@ extern int yyparse();
 extern ast_node *parsetree;
 extern FILE *yyin;
 
-enum {
-  DUMP,
-  C
-} mode;
+// -- setup for argp
+const char *argp_program_version = "zedc version beta0.1";
+const char *argp_program_bug_address = "Liam Marshall <liam@cpan.org>";
+static char doc[] = "zedc - the compiler for the Zed computer programming language";
+static char args_doc[] = "file";
+static struct argp_option options[] = {
+  {"dump",    'd', 0,   0, "Dump AST to stdout"},
+  {"stdout",  's', 0,   0, "Send .c.ir output to stdout instead of writing it to a file"},
+  {0},
+};
 
-bool doStdout = false;
+// -- struct to hold parsed arguments
+struct arguments {
+  char *args[1];
+  enum {DUMP_MODE, C_MODE} output_mode;
+  bool doStdout;
+};
+
+// -- function to parse out argp opts
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+  struct arguments *arguments = state->input;
+
+  switch (key) {
+    case 'd':
+      arguments->output_mode = DUMP_MODE;
+      break;
+    case 's':
+      arguments->doStdout = true;
+      break;
+    case ARGP_KEY_ARG:
+      if (state->arg_num > 1)
+        argp_usage(state);
+      arguments->args[state->arg_num] = arg;
+      break;
+    case ARGP_KEY_END:
+      if (state->arg_num < 1)
+        argp_usage(state);
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+
+  return 0;
+}
+
+// -- the actual argparse struct
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char** argv) {
-  mode = C; // by default, the mode should be outputting C
+  struct arguments arguments;
+
+  arguments.output_mode = C_MODE;
+  arguments.doStdout = false;
+
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
   // open the source file
-  FILE *source = fopen(argv[1], "r");
+  FILE *source = fopen(arguments.args[0], "r");
   if (!source) {
     printf("Can't open %s!\n", argv[1]);
     return 1;
@@ -43,9 +92,9 @@ int main(int argc, char** argv) {
   sds emitted_code = codegen(parsetree);
 
   // switch on the mode
-  switch (mode) {
-    case C: { // for c
-      if (doStdout) { // if we're outputting the emitted c to stdout
+  switch (arguments.output_mode) {
+    case C_MODE: { // for c
+      if (arguments.doStdout) { // if we're outputting the emitted c to stdout
         printf("%s", emitted_code);
       } else {
         // open a new file, with the name of the original file + '.ir.c'
@@ -55,7 +104,7 @@ int main(int argc, char** argv) {
       }
       break;
     }
-    case DUMP: { // for an ast dump
+    case DUMP_MODE: { // for an ast dump
       dump_ast_node(parsetree); // dump the ast to stdout. the ast dump is xml-like.
       break;
     }
